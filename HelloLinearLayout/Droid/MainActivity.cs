7fd2c132e4be102251bfcea82fb0f6946a3a8eb9
Canek García <kanect@rocketmail.com>
2016-09-07 20:49:52 -0500

    horizontal example

--- HelloLinearLayout/Droid/MainActivity.cs
+++ HelloLinearLayout/Droid/MainActivity.cs
@@ -7,7 +7,7 @@ namespace HelloLinearLayout.Droid
 	[Activity(Label = "HelloLinearLayout", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		//int count = 1;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -18,9 +18,9 @@ namespace HelloLinearLayout.Droid
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			//Button button = FindViewById<Button>(Resource.Id.myButton);
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			//button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
 		}
 	}
 }
