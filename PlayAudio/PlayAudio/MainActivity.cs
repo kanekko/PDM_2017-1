e7b975538d05746c3bae52b6994cafba899f69bc
Canek <c4n3k@live.com.mx>
2016-10-03 08:27:29 -0500

    Proyecto para reproducir audio

---
+++ PlayAudio/PlayAudio/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+
+namespace PlayAudio
+{
+    [Activity(Label = "PlayAudio", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        MediaPlayer _player;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            _player = MediaPlayer.Create(this, Resource.Raw.test);
+            _player.Start();
+
+
+        }
+    }
+}
+
