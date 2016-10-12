a7cbe7962a3fe8d95e0141255f08878a5497bd15
Canek <c4n3k@live.com.mx>
2016-10-12 18:57:26 -0500

    Write and Read file on android

---
+++ HelloFilesystem/HelloFilesystem/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using System.IO;
+using System;
+
+namespace HelloFilesystem
+{
+    [Activity(Label = "HelloFilesystem", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            //SetContentView (Resource.Layout.Main);
+
+            //internal storage
+            string path = Application.Context.FilesDir.Path;
+            var filePath = Path.Combine(path, "test.txt");
+            System.IO.File.WriteAllText(filePath, "Hello World");
+            // read from internal storage
+            var reader = System.IO.File.ReadAllText(filePath);
+            Console.WriteLine("reader:~>"+reader);
+
+            //external storage (public)
+            //string path2 = Android.OS.Environment.GetExternalStoragePublicDirectory.path;
+            //external storage (private)
+            //string path2 = Application.Context.GetExternalFilesDir;
+        }
+        
+
+
+
+    }
+    
+}
