229ef68297c98870a37ce578b40baf6255300f91
Canek <c4n3k@live.com.mx>
2016-10-12 19:22:03 -0500

    Play video example

---
+++ PlayVideo/PlayVideo/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PlayVideo
+{
+    [Activity(Label = "PlayVideo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var videoView = FindViewById<VideoView>(Resource.Id.SampleVideoView);
+
+            var uri = Android.Net.Uri.Parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+
+
+            videoView.SetVideoURI(uri);
+            videoView.Start();
+        }
+    }
+}
+
