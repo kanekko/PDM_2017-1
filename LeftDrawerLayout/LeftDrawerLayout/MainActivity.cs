00d74bf2e53a6be3389f802f75511f1c67043b41
Canek <c4n3k@live.com.mx>
2016-10-17 15:24:54 -0500

    slide menu demo

---
+++ LeftDrawerLayout/LeftDrawerLayout/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿//using Android.App;
+//using Android.Widget;
+//using Android.OS;
+
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+//using Android.Support.Design.Widget;
+
+namespace LeftDrawerLayout
+{
+    [Activity(Label = "LeftDrawerLayout", Theme = "@style/Theme.DesignDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        DrawerLayout drawerLayout;
+        //NavigationView navigationView;
+       
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.Icon);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
